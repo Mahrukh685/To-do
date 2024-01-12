89816f0e70b0518e4f3e66cc8b86cb18f9e4a66e
Mahrukh685 <mahrukhkhan685@gmail.com>
2024-01-13 03:04:48 +0500

    complete

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
-let id=0;
+let id = 0;
 
-function todo(){
-   let input1=document.getElementById("field1");
-   let container=document.getElementById("container");
-   let div=document.createElement("div");
-   let h3=document.createElement("h3");
-   let button=document.createElement("button");
+function todo() {
+   let input1 = document.getElementById("field1");
+   if(input1.value==""){
+      return
+   }
+   let container = document.getElementById("container");
+   let div = document.createElement("div");
+   let h3 = document.createElement("h3");
+   let button = document.createElement("button");
+   button.addEventListener("click",
+      Delete
+   )
    div.appendChild(h3)
    div.appendChild(button)
-   container.appendChild(div);
-   h3.textContent=input1.value;
-   button.textContent="Delete"
+   container.appendChild(div)
+   h3.textContent = input1.value;
+   button.textContent = "Delete"
+   input1.value="";
+
+
    div.classList.add("row");
-   
-   div.setAttribute('id',id)
+   div.setAttribute('id', "uniqueid" + id)
    id++;
+
+}
+
+function Delete(id) {
+   let parent=this.parentElement;
+   parent.remove();
 
 
 }
 
-   
 
 
 
 
 
 
-    
+
+
 
 
 
